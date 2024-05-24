f3886c061ed8d698f144ced4cae8077b03ab0feb
ChuvilkoDEV <ilyasutton@gmail.com>
2024-05-24 15:39:55 +0300

    Добавлена пагинация таблицы

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useMemo, useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
 import axios from 'axios';
-import { useTable, useSortBy, useResizeColumns, useFilters } from 'react-table';
-
+import { useTable, useSortBy, useResizeColumns, useFilters, usePagination } from 'react-table';
 
 export default function Table() {
   const [sessions, setSessions] = useState([]);
@@ -16,7 +15,7 @@ export default function Table() {
           { token }
         );
         if (response.data.status !== 'ok')
-          throw new Error('Что-то пошло не так...')
+          throw new Error('Что-то пошло не так...');
 
         Cookies.set('AllSessions', response.data.sessions);
         setSessions(response.data.sessions);
@@ -28,50 +27,105 @@ export default function Table() {
     fetchSessions();
   }, []);
 
+  const data = useMemo(() => sessions, [sessions]);
 
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Номер телефона',
+        accessor: 'tagName',
+      },
+      {
+        Header: 'BAN status',
+        accessor: 'ban',
+        Cell: ({ value }) => (
+          <span className={value === 'Бан' ? 'text-danger' : 'text-success'}>{value}</span>
+        ),
+      },
+      {
+        Header: 'Группа',
+        accessor: 'group',
+      },
+      {
+        Header: 'Прокси',
+        accessor: 'proxy',
+      },
+      {
+        Header: 'Дата',
+        accessor: 'dateAdd',
+      },
+    ],
+    []
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    page,
+    nextPage,
+    previousPage,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    state: { pageIndex },
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 0, pageSize: 10 }, // Установите начальные значения индекса страницы и размера страницы
+    },
+    useFilters,
+    useSortBy,
+    useResizeColumns,
+    usePagination
+  );
 
   return (
     <div className="content">
       <div className="header">
-        <h4>My account List</h4>
-        <div className="filters">
-          <button className="btn btn-primary">Всего: 6610</button>
-          <button className="btn btn-success">Работают: 3629</button>
-          <button className="btn btn-info">Восстановлено: 906</button>
-          <button className="btn btn-danger">Забанено: 2075</button>
-          <button className="btn btn-secondary">Прокси: 0</button>
-          {/* <button className="btn btn-outline-secondary"><FontAwesomeIcon icon={faSync} /></button> */}
-        </div>
+        <h4>Список сессий</h4>
       </div>
       <div className="table-responsive">
-        <table className="table table-white table-striped">
+        <table className="table table-white table-striped" {...getTableProps()}>
           <thead>
-            <tr>
-              <th><input type="checkbox" /></th>
-              <th>Номер телефона</th>
-              <th>BAN status</th>
-              <th>Группа</th>
-              <th>Прокси</th>
-              <th>Дата</th>
-            </tr>
-          </thead>
-          <tbody>
-            {sessions.map(item => (
-              <tr key={item.id}>
-                <td><input type="checkbox" /></td>
-                <td>{item.tagName}</td>
-                <td
-                  className={item.ban == 1 ? 'text-success' : 'text-danger'}>
-                  {item.ban == 1 ? 'Живой' : 'Забанен'}
-                </td>
-                <td>{item.group}</td>
-                <td>{item.proxy}</td>
-                <td>{item.dateAdd}</td>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                ))}
               </tr>
             ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {page.map((row) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => {
+                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+                  })}
+                </tr>
+              );
+            })}
           </tbody>
         </table>
+        <div className="pagination">
+          <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+            Предыдущая
+          </button>{' '}
+          <button onClick={() => nextPage()} disabled={!canNextPage}>
+            Следующая
+          </button>{' '}
+          <span>
+            Страница{' '}
+            <strong>
+              {pageIndex + 1} из {pageOptions.length}
+            </strong>{' '}
+          </span>
+        </div>
       </div>
     </div>
-  )
+  );
 }
