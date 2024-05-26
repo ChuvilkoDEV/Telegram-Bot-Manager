7328fd91b9da5994756310df7e0051e18d3eaaa8
ChuvilkoDEV <ilyasutton@gmail.com>
2024-05-26 16:54:40 +0300

    Переход на LTS render в index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,19 @@
 // src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 reportWebVitals();
